81769b4d7e98bf607e243661cfbc86d5c7747649
dongbaiqiang <dongbaiqiang@playcrab.com>
2019-09-05 18:28:05 +0800

    Desc:小游戏服务器

---
+++ z_smallGames/src/main/java/d_rpc/AddFunServiceImpl.java
@@ -0,0 +1,11 @@
+package d_rpc;
+
+public class AddFunServiceImpl implements AddFun{
+
+
+	public String add(String h) {
+		String[] sum = h.split(" "); 
+		int s = Integer.valueOf(sum[0]) + Integer.valueOf(sum[1]);
+		return String.valueOf(s);
+	}
+}
